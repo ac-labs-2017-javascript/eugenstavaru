5b8e9cddbabf2d305d3bfd8bcf31b42e3179debc
asd <asd>
2017-03-28 18:02:59 +0300

    doing stutrfff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log("Hello world");
+var express = require("express");
+var fetch = require("node-fetch");
+var cheerio = require("cheerio");
+var app = express();
+
+function processText(text) {
+	var $ = cheerio.load(text);
+	var listItems = Array.from($("#tiles").children("li"));
+	return listItems.map(function(li){
+
+		return { title : $(li).find(".title").text() };
+	
+
+	});
+}
+
+app.get("/hello", function(req, res) {
+	var dopopocoSite = fetch("http://dopopoco.ro/meniu-individual-timisoara");
+	var textResponse = dopopocoSite.then(function(response) {
+		return response.text();
+	});
+	textResponse.then(function(text){
+		res.send(processText(text));
+	});
+});
+
+app.listen(3000, function() {
+	console.log("Nu a crapat!");
+});
+
+//http://dopopoco.ro/meniu-individual-timisoara
